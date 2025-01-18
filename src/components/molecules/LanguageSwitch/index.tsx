a7a71e733b7ec372df4bd17efda2e9acd3b6fcf5
Michel Raupp de Oliveira <86214974+michel-raupp@users.noreply.github.com>
2025-01-18 02:36:26 -0300

    feat: creates select language window

--- src/components/molecules/LanguageSwitch/index.tsx
+++ src/components/molecules/LanguageSwitch/index.tsx
@@ -1,8 +1,18 @@
+import { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
 
 import { LANGUAGES, TLanguage, useI18nStore, useThemeStore } from "@/stores";
 
-import { FlagButton, FlagImg, StyledSwitch } from "./styles";
+import { TEXT_SIZE } from "@/components/atoms/Text/types";
+import { Text } from "@/components/atoms";
+import {
+  FlagButton,
+  FlagImg,
+  ImgContainer,
+  LanguageOption,
+  SelectWindow,
+  StyledSwitch,
+} from "./styles";
 
 import PT_FLAG from "@/assets/flags/pt-BR.png";
 import EN_FLAG from "@/assets/flags/en-US.png";
@@ -18,36 +28,89 @@ interface IData {
   english: ILang;
 }
 
-interface IFlag {
+interface IOption {
   data: IData;
   language: TLanguage;
-  choseLanguage: (value: TLanguage) => void;
   isDarkMode: boolean;
+  choseLanguage: (value: TLanguage) => void;
+  handleCloseWindow: () => void;
 }
 
-const Flag = ({ data, choseLanguage, isDarkMode, language }: IFlag) => {
+const Option = ({
+  data,
+  isDarkMode,
+  language,
+  choseLanguage,
+  handleCloseWindow,
+}: IOption) => {
+  function handleClickOutside(event: any) {
+    if (event.target.closest("button")) return;
+    handleCloseWindow();
+  }
+  useEffect(() => {
+    document.addEventListener("click", handleClickOutside);
+    return () => {
+      document.removeEventListener("click", handleClickOutside);
+    };
+  }, []);
+
   if (language === LANGUAGES.PT) {
     return (
-      <FlagButton
-        onClick={() => choseLanguage(LANGUAGES.EN)}
+      <LanguageOption
         $darkMode={isDarkMode}
+        onClick={() => choseLanguage(LANGUAGES.EN)}
       >
+        <ImgContainer $darkMode={isDarkMode}>
+          <FlagImg
+            src={data.english.image}
+            alt={data.english.label}
+            $darkMode={isDarkMode}
+          />
+        </ImgContainer>
+        <Text text={data.english.label} size={TEXT_SIZE.S} />
+      </LanguageOption>
+    );
+  }
+
+  return (
+    <LanguageOption
+      $darkMode={isDarkMode}
+      onClick={() => choseLanguage(LANGUAGES.PT)}
+    >
+      <ImgContainer $darkMode={isDarkMode}>
         <FlagImg
           src={data.portuguese.image}
           alt={data.portuguese.label}
           $darkMode={isDarkMode}
         />
-      </FlagButton>
-    );
-  }
+      </ImgContainer>
+      <Text text={data.portuguese.label} size={TEXT_SIZE.S} />
+    </LanguageOption>
+  );
+};
+
+interface ILanguageButton {
+  language: TLanguage;
+  isDarkMode: boolean;
+  handleOpen: () => void;
+  t: any;
+}
+
+const LanguageButton = ({
+  language,
+  isDarkMode,
+  handleOpen,
+  t,
+}: ILanguageButton) => {
   return (
-    <FlagButton
-      onClick={() => choseLanguage(LANGUAGES.PT)}
-      $darkMode={isDarkMode}
-    >
+    <FlagButton onClick={handleOpen} $darkMode={isDarkMode}>
       <FlagImg
-        src={data.english.image}
-        alt={data.english.label}
+        src={language === LANGUAGES.PT ? PT_FLAG : EN_FLAG}
+        alt={
+          language === LANGUAGES.PT
+            ? t("languagePortuguese")
+            : t("languageEnglish")
+        }
         $darkMode={isDarkMode}
       />
     </FlagButton>
@@ -57,8 +120,8 @@ const Flag = ({ data, choseLanguage, isDarkMode, language }: IFlag) => {
 const LanguageSwitch = () => {
   const { isDarkMode } = useThemeStore((state) => state);
   const { updateLanguage, language } = useI18nStore((state) => state);
-
   const { t } = useTranslation("default");
+  const [isOpen, setIsOpen] = useState(false);
 
   const translates = {
     portuguese: {
@@ -73,18 +136,38 @@ const LanguageSwitch = () => {
     },
   };
 
+  const handleOpen = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const handleCloseWindow = () => {
+    setIsOpen(false);
+  };
+
   const choseLanguage = (value: TLanguage) => {
     updateLanguage(value);
+    handleCloseWindow();
   };
 
   return (
     <StyledSwitch $darkMode={isDarkMode}>
-      <Flag
-        data={translates}
+      <LanguageButton
         language={language}
-        choseLanguage={choseLanguage}
         isDarkMode={isDarkMode}
+        handleOpen={handleOpen}
+        t={t}
       />
+      {isOpen && (
+        <SelectWindow $darkMode={isDarkMode} tabIndex={0}>
+          <Option
+            data={translates}
+            language={language}
+            choseLanguage={choseLanguage}
+            isDarkMode={isDarkMode}
+            handleCloseWindow={handleCloseWindow}
+          />
+        </SelectWindow>
+      )}
     </StyledSwitch>
   );
 };
--- src/components/molecules/LanguageSwitch/styles.ts
+++ src/components/molecules/LanguageSwitch/styles.ts
@@ -8,7 +8,16 @@ interface IProps {
 export const StyledSwitch = styled.div<IProps>`
   display: flex;
   align-items: center;
-  justify-content: center;
+  position: relative;
+`;
+
+export const ImgContainer = styled.div<IProps>`
+  overflow: hidden;
+  width: 24px;
+  height: 24px;
+  border-radius: 50%;
+  border: 1px solid
+    ${({ $darkMode }) => ($darkMode ? DARK_THEME.lightGray : LIGHT_THEME.gray)};
 `;
 
 export const FlagButton = styled.button<IProps>`
@@ -20,7 +29,8 @@ export const FlagButton = styled.button<IProps>`
   cursor: pointer;
   border: 1px solid
     ${({ $darkMode }) => ($darkMode ? DARK_THEME.lightGray : LIGHT_THEME.gray)};
-  transition: border 0.3s;
+  transition: border-width 0.3s;
+
   &:hover {
     border: 3px solid
       ${({ $darkMode }) =>
@@ -33,14 +43,32 @@ export const FlagImg = styled.img<IProps>`
   transition: 0.3s;
 `;
 
+export const LanguageOption = styled.button<IProps>`
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  gap: 8px;
+  width: 100%;
+  padding: 8px;
+  border-radius: 6px;
+  background-color: transparent;
+  transition: background-color 0.3s;
+
+  &:hover {
+    background-color: ${({ $darkMode }) =>
+      $darkMode ? DARK_THEME.lightGray : LIGHT_THEME.lightGray};
+  }
+`;
+
 export const SelectWindow = styled.div<IProps>`
   display: flex;
   flex-direction: column;
   position: absolute;
-  top: 48px;
+  top: 32px;
   right: 0;
   z-index: 10;
-  width: 100px;
+  padding: 4px;
+  border-radius: 8px;
   background-color: ${({ $darkMode }) =>
     $darkMode ? DARK_THEME.gray : LIGHT_THEME.background};
   border: 1px solid
