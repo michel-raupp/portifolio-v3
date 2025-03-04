f2a630248c58b954eb982f10e451a51c5df468ba
Michel Raupp de Oliveira <86214974+michel-raupp@users.noreply.github.com>
2025-03-04 16:13:37 -0300

    feat: add skeletons to about components

--- src/components/molecules/ActionChat/index.tsx
+++ src/components/molecules/ActionChat/index.tsx
@@ -1,10 +1,11 @@
+import { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
 import { IconDownload } from "@tabler/icons-react";
 
 import { useThemeStore } from "@/stores";
 import { DARK_THEME, LIGHT_THEME, THEME_COLORS } from "@/constants";
 
-import { Socials, Text } from "@/components/atoms";
+import { Skeleton, Socials, Text } from "@/components/atoms";
 
 import { Chat, DownloadButton, SocialWrapper } from "./style";
 
@@ -15,28 +16,37 @@ interface IActionChat {
 const ActionChat = ({ text }: IActionChat) => {
   const { isDarkMode } = useThemeStore((state) => state);
   const { t } = useTranslation("commons");
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setLoading(false);
+  }, []);
 
   return (
     <Chat $darkMode={isDarkMode}>
       <Text text={text} color={THEME_COLORS.lightText} />
-      <SocialWrapper>
-        <Socials />
-        <DownloadButton
-          $darkMode={isDarkMode}
-          href="/resume.pdf"
-          download
-          aria-label={t("resume")}
-        >
-          <Text
-            text={t("resume")}
-            color={isDarkMode ? THEME_COLORS.text : THEME_COLORS.background}
-          />
-          <IconDownload
-            size={24}
-            color={isDarkMode ? DARK_THEME.text : LIGHT_THEME.background}
-          />
-        </DownloadButton>
-      </SocialWrapper>
+      {loading ? (
+        <Skeleton height="48px" />
+      ) : (
+        <SocialWrapper>
+          <Socials />
+          <DownloadButton
+            $darkMode={isDarkMode}
+            href="/resume.pdf"
+            download
+            aria-label={t("resume")}
+          >
+            <Text
+              text={t("resume")}
+              color={isDarkMode ? THEME_COLORS.text : THEME_COLORS.background}
+            />
+            <IconDownload
+              size={24}
+              color={isDarkMode ? DARK_THEME.text : LIGHT_THEME.background}
+            />
+          </DownloadButton>
+        </SocialWrapper>
+      )}
     </Chat>
   );
 };
--- src/components/sections/About/index.tsx
+++ src/components/sections/About/index.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
 import { IconDownload } from "@tabler/icons-react";
 
@@ -5,7 +6,7 @@ import { DARK_THEME, LIGHT_THEME, THEME_COLORS } from "@/constants";
 import { useThemeStore } from "@/stores";
 
 import { TEXT_HIERARCHIES, TEXT_SIZE } from "@/components/atoms/Text/types";
-import { Text } from "@/components/atoms";
+import { Skeleton, Text } from "@/components/atoms";
 
 import { getSkillsData } from "./data";
 import {
@@ -21,6 +22,11 @@ const About = () => {
   const { isDarkMode } = useThemeStore((state) => state);
   const { t } = useTranslation("about");
   const skills = getSkillsData(isDarkMode);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setLoading(false);
+  }, []);
 
   return (
     <Wrapper id="about">
@@ -30,23 +36,35 @@ const About = () => {
           text={t("aboutTitle1")}
           color={THEME_COLORS.text}
         />
-        <Text text={t("aboutText1")} color={THEME_COLORS.lightText} />
-        <Text text={t("aboutText2")} color={THEME_COLORS.lightText} />
-        <DownloadButton
-          href="/resume.pdf"
-          download
-          aria-label={t("downloadResume")}
-          $darkMode={isDarkMode}
-        >
-          <Text
-            text={t("downloadResume")}
-            color={isDarkMode ? THEME_COLORS.text : THEME_COLORS.background}
-          />
-          <IconDownload
-            size={24}
-            color={isDarkMode ? DARK_THEME.text : LIGHT_THEME.background}
-          />
-        </DownloadButton>
+        {loading ? (
+          <Skeleton height="72px" />
+        ) : (
+          <Text text={t("aboutText1")} color={THEME_COLORS.lightText} />
+        )}
+        {loading ? (
+          <Skeleton height="96px" />
+        ) : (
+          <Text text={t("aboutText2")} color={THEME_COLORS.lightText} />
+        )}
+        {loading ? (
+          <Skeleton height="40px" />
+        ) : (
+          <DownloadButton
+            href="/resume.pdf"
+            download
+            aria-label={t("downloadResume")}
+            $darkMode={isDarkMode}
+          >
+            <Text
+              text={t("downloadResume")}
+              color={isDarkMode ? THEME_COLORS.text : THEME_COLORS.background}
+            />
+            <IconDownload
+              size={24}
+              color={isDarkMode ? DARK_THEME.text : LIGHT_THEME.background}
+            />
+          </DownloadButton>
+        )}
       </Col>
 
       <Col>
