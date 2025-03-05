1825167c3b5ec2afe8b3113e37b05eefdd4989b3
Michel Raupp de Oliveira <86214974+michel-raupp@users.noreply.github.com>
2025-03-05 12:02:22 -0300

    feat: remove tag helmet

--- src/components/atoms/index.ts
+++ src/components/atoms/index.ts
@@ -7,4 +7,3 @@ export { default as Tooltip } from "./Tooltip";
 export { default as Section } from "./Section";
 export { default as Footer } from "./Footer";
 export { default as Skeleton } from "./Skeleton";
-export { default as MetaTags } from "./MetaTags";
--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,7 @@
 import { StrictMode, Suspense, lazy } from "react";
 import { createRoot } from "react-dom/client";
-import { HelmetProvider } from "react-helmet-async";
 
-import { MetaTags, Skeleton } from "./components/atoms";
+import { Skeleton } from "./components/atoms";
 import { I18nProvider, ThemeColorsProvider } from "./providers";
 
 const App = lazy(() => import("./App.tsx"));
@@ -12,16 +11,13 @@ import "./locales";
 export const Root = () => {
   return (
     <StrictMode>
-      <HelmetProvider>
-        <I18nProvider>
-          <ThemeColorsProvider>
-            <MetaTags />
-            <Suspense fallback={<Skeleton height="100vh" width="100vw" />}>
-              <App />
-            </Suspense>
-          </ThemeColorsProvider>
-        </I18nProvider>
-      </HelmetProvider>
+      <I18nProvider>
+        <ThemeColorsProvider>
+          <Suspense fallback={<Skeleton height="100vh" width="100vw" />}>
+            <App />
+          </Suspense>
+        </ThemeColorsProvider>
+      </I18nProvider>
     </StrictMode>
   );
 };
