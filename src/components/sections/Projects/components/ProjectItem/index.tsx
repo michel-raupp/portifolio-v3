8d39f608b8b474b08e65453c1217c2a5bb721b0f
Michel Raupp de Oliveira <86214974+michel-raupp@users.noreply.github.com>
2025-03-05 17:07:56 -0300

    fix: remove unused imports

--- src/components/sections/Projects/components/ProjectItem/index.tsx
+++ src/components/sections/Projects/components/ProjectItem/index.tsx
@@ -1,4 +1,4 @@
-import { Suspense, lazy, useEffect, useState } from "react";
+import { Suspense } from "react";
 import { useTranslation } from "react-i18next";
 
 import { useThemeStore } from "@/stores";
